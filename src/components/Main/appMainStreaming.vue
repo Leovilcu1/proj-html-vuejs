5d69393253474005cc650da95012802774540ba8
LeonidVilcu <vilcu.leo@mail.ru>
2023-02-14 16:57:25 +0100

    proj-html-vuejs

--- src/components/Main/appMainStreaming.vue
+++ src/components/Main/appMainStreaming.vue
@@ -6,12 +6,28 @@
 
 
 <template>
-        <div>
-             appMainStreaming   
+        <div class="section-streaming text-light ">
+             <div class="container d-flex justify-content-center text-center flex-wrap">
+                <div class="w-50">
+                        <p class="">live Streaming</p>
+                        <h2>Lorem, ipsum dolor.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, blanditiis. Mollitia voluptatem praesentium doloribus placeat.</p>
+                </div>
+                <div class="w-75">      
+                        <img src="../../assets/img/video-bg.png" alt="" class="w-100">
+                </div>
+
+             </div>  
         </div>
 </template>
 
 
 <style lang="scss" scoped>
+        .section-streaming{
+                background-image: url(../../assets/img/live-streaming-bg.png);
+                background-size: cover;
+                background-position: center;
 
+
+        }
 </style>
